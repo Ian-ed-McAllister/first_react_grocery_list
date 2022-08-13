91cc13fb690b95578e1ba7da329c44e96a6024ae
Ian <ian.ed.mcallister@gmail.com>
2022-08-13 16:06:16 -0700

    Chapter 7 Complete

--- src/Content.js
+++ src/Content.js
@@ -1,35 +1,89 @@
 import React from 'react'
+import { useState } from 'react'
+import {FaTrashAlt} from "react-icons/fa"
+
 
 const Content = () => {
-    const handleNameChange = () => {
-        const names = ["Bob", "Dave", "Kevin"]
-        const int = Math.floor(Math.random()*3)
-        return names[int]
+    // we use const as we never want to reset the "name" again directly
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: false,
+            item: "1/2 pound bag of cocoa Almonds"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Item2"
+        },
+        {
+            id:3,
+            checked: false,
+            item: "item 3"
+        }
+    ])
+
+    const handleCheck = (id) => {
+        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked} : item)
+        setItems(listItems)
+        localStorage.setItem('shoppingList', JSON.stringify(listItems))
+
+        console.log(`key: ${id}`)
     }
 
-    const handleClick = () =>{
-        console.log("CLICKED")
+    const handleDelete = (id) =>{
+        const listItems = items.filter((item) => item.id !== id)
+        setItems(listItems)
+        localStorage.setItem('shoppingList', JSON.stringify(listItems))
     }
-    const handleClick2 = (name) =>{
-        console.log(`${name} was clicked`)
-    }
-    const handleClick3 = (event) =>{
-        console.log(event.target.innerText)
-    }
+
+
+    // LEARNING ABOUT USE STATE
+
+    // const handleClick = () =>{
+    //     // here it will still output 0 the first time you click it as it uses the count value when it enters the function
+    //     setCount(count + 1)
+    //     //Even when adding again it will set count to what count was comming into the function +1 not +2
+    //     setCount(count + 1)
+    //     console.log(count)
+    //     // This is because the value of count within the function WILL NOT GET CHANGED, the value outside does and when you call the function again the local value gets set to the outside value
+    // }
+    // const handleClick2 = (name) =>{
+    //     console.log(`${name} was clicked`)
+    // }
+    // const handleClick3 = (event) =>{
+    //     console.log(event.target.innerText)
+    // }
 
   return (
     <main>
-        <p onDoubleClick={handleClick}>
-            Hello {handleNameChange()}
-        </p>
-        <button onClick={handleClick}>Click Me</button>
-        {/* The () is an annonmys function making it not get called immediatly when the page is loaded liek what would happen
-         if you were just to do handleClick2(handleNameChange())*/}
-        <button onClick={() => handleClick2(handleNameChange())}>Click Me</button>
-
-
-        <button onClick={(event) => handleClick3(event)}>Click Me</button>
-
+        {items.length ? (
+            <ul>
+                {items.map((item) =>(
+                    //list items need to have the key item to allow react to know if it got changed
+                    <li className='item' key={item.id} onDoubleClick={() => handleCheck(item.id)}>
+                        <input 
+                            type="checkbox" 
+                            checked={item.checked}
+                            onChange = {() => handleCheck(item.id)}
+                        />
+                        <label
+                            style={(item.checked) ? {textDecoration: 'line-through'}: null}
+                            onDoubleClick={() => handleCheck(item.id)}
+                        >{item.item}</label>
+                        <FaTrashAlt 
+                            role="button" 
+                            tabIndex="0"
+                            onClick={() => handleDelete(item.id)}
+                        />
+                    </li>
+                ))}
+            </ul>
+        ) : (
+            <p style={{marginTop: '2rem'}}>
+                NO ITEMS IN LIST
+            </p>
+        )}
     </main>
   )
 }
